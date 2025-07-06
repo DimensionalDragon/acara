bcdf2c8176df156d17ae73412ef95163b1f77258
Andhika Satrya <andhikangurah@gmail.com>
2025-07-06 23:01:24 +0800

    feat: add root endpoint for server health check

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,6 +15,10 @@ async function init() {
 
         app.use(bodyParser.json());
 
+        app.get('/', (req, res) => res.status(200).json({
+            message: 'Server is running',
+            data: null,
+        }));
         app.use('/api', router);
 
         app.listen(PORT, () => {
