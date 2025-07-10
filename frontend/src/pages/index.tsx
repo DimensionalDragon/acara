9f0abb89d8a986a7d5210be50d51cb6bc439d17f
Andhika Satrya <andhikangurah@gmail.com>
2025-07-10 18:19:31 +0800

    feat: init frontend project

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import PageHead from "@/components/commons/PageHead";
+import { Button } from "@nextui-org/react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <PageHead />
+      <Button color="primary">
+        Hello World!
+      </Button>
+    </main>
+  );
+}
