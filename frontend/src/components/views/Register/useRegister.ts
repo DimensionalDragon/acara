043ce82a9fe6583197f8698d2f4a76ccbbbab77a
Andhika Satrya <andhikangurah@gmail.com>
2025-07-15 04:57:31 +0800

    patch: fix import of auth types

--- frontend/src/components/views/Register/useRegister.ts
+++ frontend/src/components/views/Register/useRegister.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { IRegister } from "@/types/auth";
+import { IRegister } from "@/types/Auth";
 import authServices from "@/services/auth";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/router";
--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -1,7 +1,7 @@
 import instance from "@/libs/axios/instance";
 import endpoint from "./endpoint.constant";
 
-import { IRegister } from "@/types/auth";
+import { IRegister } from "@/types/Auth";
 
 const authServices = {
     register: async (payload: IRegister) => {
